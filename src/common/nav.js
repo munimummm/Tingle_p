d155018b00dda6c17c7f89e44ee1595895a131b2
munimummm <moom6857@naver.com>
2023-01-03 01:25:53 +0900

    전체 글꼴,사이드바 디자인 변경

--- src/common/nav.js
+++ src/common/nav.js
@@ -1,8 +1,17 @@
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
-import { Search, Recommend, BarChart, Storage } from "@mui/icons-material";
+import { NavLink, useNavigate } from "react-router-dom";
+import {
+  Search,
+  Recommend,
+  BarChart,
+  Storage,
+  SupportAgent,
+  ManageAccounts,
+  AccountCircle,
+} from "@mui/icons-material";
+import { Button, ButtonGroup, TextField, InputAdornment } from "@mui/material";
 
-let SidebarWrapper = styled.div`
+let SidebarContainer = styled.div`
   position: fixed;
   top: 0;
   width: 250px;
@@ -13,123 +22,140 @@ let SidebarWrapper = styled.div`
   font-family: "Noto Sans KR", sans-serif;
   font-weight: 300;
   z-index: 1;
+  color: ${(props) => props.theme.subColor};
 `;
-let SidebarNav = styled.ul`
+let SidebarNav = styled.div`
   width: 250px;
-  margin: 0;
-  padding: 0;
-  list-style: none;
+  button {
+    justify-content: flex-start;
+    color: ${(props) => props.theme.subColor};
+    width: 100%;
+    padding: 10px 18px 10px 24px;
+  }
 `;
 
-let SidebarBrand = styled.li`
-  font-size: 1.7rem;
+let SidebarBrand = styled.div`
+  font-size: 2rem;
   line-height: 2.5em;
+  padding: 10px 18px 10px 24px;
 `;
-let Logo = styled.a`
-  text-indent: 26px;
+let Logo = styled(NavLink)`
   font-family: "Carter One", cursive;
-  color: #9147ff;
+  color: ${(props) => props.theme.mainColor};
 `;
 let MenuAll = styled.div`
   font-size: 1.2rem;
-  input {
-    color: white;
-  }
-
-  li {
-    margin: 5px;
-    text-indent: 1.5rem;
-    line-height: 2em;
-  }
-  a {
-    display: block;
-    text-decoration: none;
-    color: #cccccc;
+  padding: 0;
+  .btnGroup {
+    width: 100%;
+    padding: 10px 0px 10px 0px;
   }
 `;
-
+let SearchTextField = styled(TextField)`
+  & .MuiInputBase-root {
+    margin-left: 10px;
+    width: 80%;
+    background: #1a1a1a;
+    border-color: white;
+    color: ${(props) => props.theme.subColor};
+  }
+`;
 function Nav() {
-  const clickStyle = {
-    color: "blue",
+  const navigate = useNavigate();
+  const searchOnkeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (e.target.value !== null) {
+        console.log(e.target.value);
+        navigate("/search", {
+          state: {
+            value: e.target.value,
+          },
+        });
+      }
+    }
   };
+
   return (
-    <SidebarWrapper>
+    <SidebarContainer>
       <SidebarNav>
         <SidebarBrand>
           <Logo>Tingle</Logo>
         </SidebarBrand>
+        <Button
+          startIcon={<AccountCircle />}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          로그인
+        </Button>
 
-        {/* <c:choose>
-                    <c:when test="${userData == null}">
-                        <li class="login"><a href="<c:url value="/user/UserLogIn"/>">
-                            <span class="material-symbols-outlined">account_circle</span>로그인</a></li>
-                    </c:when>
-                    <c:otherwise>
-                        <li class="login"><a href="<c:url value="/user/UserLogOut"/>">
-                            <span class="material-symbols-outlined">account_circle</span>로그아웃</a></li>
-                    </c:otherwise>
-                </c:choose> */}
         <MenuAll>
-          <NavLink>
-            <fieldset className="find_fieldset">
-              <Search></Search>
-              <input
-                spellcheck="false"
-                className="findEnter find-input iptxt"
-                type="text"
-                placeholder="검색"
-              />
-            </fieldset>
-          </NavLink>
-          <li className="btncolor">
-            <NavLink
-              className="suggestion"
-              to="/recommend"
-              style={({ isActive }) => (isActive ? clickStyle : undefined)}
+          <SearchTextField
+            onKeyPress={searchOnkeyPress}
+            margin="dense"
+            size="small"
+            placeholder="검색"
+            color="primary"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start" sx={{ color: "white" }}>
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            variant="outlined"
+          />
+
+          <ButtonGroup
+            orientation="vertical"
+            variant="string"
+            className="btnGroup"
+          >
+            <Button
+              startIcon={<Recommend />}
+              onClick={() => {
+                navigate("/recommend");
+              }}
             >
-              <Recommend></Recommend>추천
-            </NavLink>
-          </li>
-          <li className="btncolor">
-            <NavLink
-              className="chart"
-              to="/chart"
-              style={({ isActive }) => (isActive ? clickStyle : undefined)}
+              추천
+            </Button>
+            <Button
+              startIcon={<BarChart />}
+              onClick={() => {
+                navigate("/chart");
+              }}
             >
-              <BarChart></BarChart>차트
-            </NavLink>
-          </li>
-          <li className="btncolor">
-            <NavLink
-              className="library"
-              to="/storage"
-              style={({ isActive }) => (isActive ? clickStyle : undefined)}
+              차트
+            </Button>
+            <Button
+              startIcon={<Storage />}
+              onClick={() => {
+                navigate("/storage");
+              }}
             >
-              <Storage />
               보관함
-            </NavLink>
-          </li>
-          <li className="btncolor">
-            <NavLink
-              className="board"
-              to="/helpCenter"
-              style={({ isActive }) => (isActive ? clickStyle : undefined)}
+            </Button>
+            <Button
+              startIcon={<SupportAgent />}
+              onClick={() => {
+                navigate("/helpCenter");
+              }}
             >
               고객센터
-            </NavLink>
-          </li>
-          <li className="btncolor">
-            <NavLink
-              className="myPage"
-              to="/myPage"
-              style={({ isActive }) => (isActive ? clickStyle : undefined)}
+            </Button>
+            <Button
+              startIcon={<ManageAccounts />}
+              onClick={() => {
+                navigate("/myPage");
+              }}
             >
               마이페이지
-            </NavLink>
-          </li>
+            </Button>
+          </ButtonGroup>
         </MenuAll>
       </SidebarNav>
-    </SidebarWrapper>
+    </SidebarContainer>
   );
 }
 export default Nav;
