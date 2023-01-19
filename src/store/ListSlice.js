858020829becc8763351d94cea27109a3c988b00
munimummm <moom6857@naver.com>
2023-01-20 00:52:49 +0900

    ListSlice생성(useState에서 useSelector로 변경)

---
+++ src/store/ListSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+let listSlice = createSlice({
+  name: "list",
+  initialState: { list: [] },
+  reducers: {
+    setChartLists(state, action) {
+      state.list = action.payload;
+    },
+  },
+});
+
+export const { setChartLists } = listSlice.actions;
+export default listSlice;
