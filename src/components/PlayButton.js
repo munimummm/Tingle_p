a3230044068b7d3d5aab8a1b18a2e3ca5f9cae0e
munimummm <moom6857@naver.com>
2023-02-08 20:43:13 +0900

    플레이버튼 클릭시 플레이리스트 추가

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,11 +1,14 @@
 import { ImageAspectRatio, MoreVert, PlayArrow } from "@mui/icons-material";
 import { useState } from "react";
-
-function PlayButton({ onPlay }) {
+import { useDispatch, useSelector } from "react-redux";
+import { setAddPlayList } from "store/AudioSlice";
+function PlayButton({ onPlay, list }) {
+  const dispatch = useDispatch();
   return (
     <PlayArrow
       onClick={() => {
         onPlay();
+        dispatch(setAddPlayList(list));
       }}
     ></PlayArrow>
   );
