21aab64b3adae9efa9b934b742410c824c9b962f
munimummm <moom6857@naver.com>
2023-02-24 19:41:45 +0900

    고객센터  완성

---
+++ src/api/boardData.js
@@ -0,0 +1,17 @@
+const boardData = [
+  {
+    id: 0,
+    title: "제목입니다",
+    type: "공지사항",
+    content: "내용입니다",
+    date: "2021년 3월 6일",
+  },
+  {
+    id: 1,
+    title: "제목입니다2",
+    type: "공지사항",
+    content: "내용입니다2",
+    date: "2021년 3월 6일",
+  },
+];
+export default boardData;
--- src/pages/helpCenter.js
+++ src/pages/helpCenter.js
@@ -12,42 +12,92 @@ import {
   Button,
   ButtonGroup,
 } from "@mui/material";
-const HeplContainer = styled.div`
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import NoticeModal from "components/NoticeModal";
+import boardData from "api/boardData";
+const HelpContainer = styled.div`
   width: 100%;
-  height: 130px;
   margin-bottom: 10px;
+  align-items: center;
+  .helpHeader {
+    h1 {
+      margin-bottom: 30px;
+    }
+    h2 {
+      margin: 20px 0px 10px 0px;
+    }
+  }
+  .modal {
+    width: 100%;
+    background: #f5f5f5;
+  }
 `;
 function HelpCenter() {
+  const [modal, setModal] = useState(false);
+  const [board] = useState(boardData);
+  const [index, setIndex] = useState(0);
   return (
-    <HeplContainer>
-      <h1>고객센터</h1>
-      <ButtonGroup variant="contained">
-        <Button>공지사항</Button>
-        <Button>FAQ</Button>
-        <Button>1:1문의</Button>
-      </ButtonGroup>
-      <h2>공지사항</h2>
-      <TableContainer component={Paper}>
+    <HelpContainer>
+      <div className="helpHeader">
+        <h1>고객센터</h1>
+        <ButtonGroup variant="contained" color="success">
+          <Button>공지사항</Button>
+          <Button>FAQ</Button>
+          <Button>1:1문의</Button>
+        </ButtonGroup>
+        <h2>공지사항</h2>
+      </div>
+      <TableContainer>
         <Table sx={{ minWidth: 650 }}>
           <TableHead>
-            <TableRow style={{ display: "flex" }}>
+            <TableRow>
               <TableCell>번호</TableCell>
               <TableCell>분류</TableCell>
               <TableCell>제목</TableCell>
-              <TableCell align="right">날짜</TableCell>
+              <TableCell align="center">날짜</TableCell>
+              <TableCell></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>1</TableCell>
-              <TableCell align="right">공지사항</TableCell>
-              <TableCell align="right">안녕하세요 제목입니다</TableCell>
-              <TableCell align="right">2021년 2월 3일</TableCell>
-            </TableRow>
+            {board.map((list, i) => (
+              <>
+                <TableRow
+                  onClick={() => {
+                    setIndex(i);
+                    setModal(!modal);
+                  }}
+                >
+                  {" "}
+                  <TableCell>{i + 1}</TableCell>
+                  <TableCell>{list.type}</TableCell>
+                  <TableCell>{list.title}</TableCell>
+                  <TableCell align="center">{list.date}</TableCell>
+                  <TableCell>
+                    {modal === true ? (
+                      i === index ? (
+                        <ExpandLess />
+                      ) : (
+                        <ExpandMore />
+                      )
+                    ) : (
+                      <ExpandMore />
+                    )}
+                  </TableCell>
+                </TableRow>
+                {modal === true && i === index ? (
+                  <TableRow className="modal">
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell colSpan={5}>{list.content}</TableCell>
+                  </TableRow>
+                ) : null}
+              </>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
-    </HeplContainer>
+    </HelpContainer>
   );
 }
+
 export default HelpCenter;
