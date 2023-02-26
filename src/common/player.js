5a61d3d4ea9e7b0542d77322e827d30bf8cee7c5
munimummm <moom6857@naver.com>
2023-02-27 00:41:26 +0900

    플레이어 스타일 변경

--- src/common/player.js
+++ src/common/player.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import React from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
-import { FormatListBulleted, Info, PlayArrow } from "@mui/icons-material";
+import { FormatListBulleted, PlayArrow, Pause } from "@mui/icons-material";
 import { useSelector, useDispatch } from "react-redux";
 import PlayList from "./PlayList";
 import { useState } from "react";
@@ -10,24 +10,59 @@ import { useState } from "react";
 import { AudioActions } from "store/AudioSlice";
 import { useRef } from "react";
 import { useEffect } from "react";
+import { setPlayListOpen } from "store/PlayListSlice";
+const PlayerContainer = styled.div`
+  width: 100%;
+  height: 100px;
+  background: #131313;
+  z-index: 100;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  align-content: center;
+  color: #fff;
+
+  img {
+    width: 82px;
+    height: 60px;
+    margin: 15px;
+  }
+  .player-text {
+    width: 300px;
+    color: #fff;
+    line-height: 30px;
+    font-size: 14px;
+    vertical-align: top;
+    margin-top: 18px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .playListIcon {
+    color: #868686;
+  }
+  .onPlayList {
+    color: #fff;
+  }
+`;
 
 function Player() {
   const playerRef = useRef();
-  const dispatch = useDispatch();
+  let dispatch = useDispatch();
+  let playListOpen = useSelector((state) => state.playList.playListOpen);
+  const isPlaying = useSelector((state) => state.audio.isPlaying);
   let file_path = useSelector((state) => state.audio.file_path);
   let cover_img = useSelector((state) => state.audio.cover_img);
   let title = useSelector((state) => state.audio.title);
   let artist = useSelector((state) => state.audio.artist);
   let currentIndex = useSelector((state) => state.audio.currentIndex);
   let listSongs = useSelector((state) => state.audio.listSongs);
-  const isPlaying = useSelector((state) => state.audio.isPlaying);
-  const [playListOpen, setPlayListOpen] = useState(false);
+  let playerPlaying = useSelector((state) => state.audio.playerPlaying);
 
   // if (!file_path) {
   //   return null;
   // }
-  console.log(listSongs.length);
-  console.log("currentIndex" + currentIndex);
 
   const handleClickPrevious = () => {
     dispatch(AudioActions.setCurrentIndexPrevious());
@@ -68,16 +103,19 @@ function Player() {
           volume={0.5}
           onPlay={() => dispatch(AudioActions.setIsPlaying(true))}
           onPause={() => dispatch(AudioActions.setIsPlaying(false))}
+          customIcons={{
+            play: <PlayArrow sx={{ fontSize: "52px" }} />,
+            pause: <Pause sx={{ fontSize: "52px" }} />,
+          }}
         ></AudioPlayer>
         <FormatListBulleted
           style={{
             width: "36px",
             height: "36px",
             margin: "36px",
-            color: "#868686",
           }}
           onClick={() => {
-            setPlayListOpen(!playListOpen);
+            dispatch(setPlayListOpen(!playListOpen));
           }}
         />
         {playListOpen === true ? <PlayList /> : null}
@@ -86,33 +124,3 @@ function Player() {
   );
 }
 export default Player;
-
-const PlayerContainer = styled.div`
-  width: 100%;
-  height: 100px;
-  background: #131313;
-  z-index: 2;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  display: flex;
-  align-content: center;
-  color: #fff;
-
-  img {
-    width: 82px;
-    height: 60px;
-    margin: 15px;
-  }
-  .player-text {
-    width: 300px;
-    color: #fff;
-    line-height: 30px;
-    font-size: 14px;
-    vertical-align: top;
-    margin-top: 18px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-`;
