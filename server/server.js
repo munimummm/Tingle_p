e131277f6df5bc0024bdba1e7bd9f55b600473db
munimummm <moom6857@naver.com>
2023-06-19 00:03:48 +0900

    Fix Vite 설정

--- server/server.js
+++ server/server.js
@@ -3,17 +3,25 @@ const path = require("path");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 let db;
 
 require("dotenv").config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/dist")));
 
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
 MongoClient.connect(process.env.DB_URL, function (error, client) {
@@ -30,7 +38,11 @@ app.use("/", require("./routes/Chart.js"));
 app.use("/detailList", require("./routes/Detail.js"));
 app.use("/", require("./routes/HelpCenter.js"));
 app.use("/searchList", require("./routes/Search.js"));
+app.use("/", require("./routes/register.js"));
 
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
