4fac0300cb37177f378ca92307840a2fcd8831c3
munimummm <moom6857@naver.com>
2023-06-19 00:04:48 +0900

    Add 회원가입 폼생성

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,19 +2,55 @@ import { Button, Checkbox, FormControlLabel, Grid } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useState } from "react";
+import { commonAxios } from "api/CommonAxios";
 const RegisterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
   padding: 0 20px;
-
    div {
     max-width: 500px;
  
   }
   `;
 function Register() {
+  const [usernameReg, setUsernameReg] = useState('');
+  const [idReg, setIdReg] = useState('');
+	const [passwordReg, setPasswordReg] = useState('');
+  const [passwordConfirmReg, setPasswordConfirmReg] = useState('');
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+
+
+  const onChangePasswordConfirm = (e) => {
+    const currentPasswordConfirm = e.target.value;
+    setPasswordConfirmReg(currentPasswordConfirm);
+    if (passwordReg !== currentPasswordConfirm) {
+      setIsPasswordConfirm(false);
+    } else {
+      setIsPasswordConfirm(true);
+    }
+  };
+
+  const userRegister = async (e) => {
+    try {
+      e.preventDefault();
+      if (!isPasswordConfirm) {
+        return alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+        
+      }
+      const result = await commonAxios.post(`/register`, {
+        username: usernameReg,
+        password: passwordReg,
+        id: idReg
+      });
+
+      console.log(result.data); // 성공 응답
+    } catch (error) {
+      console.error(error.response.data); // 오류 응답
+    }
+  }
   return (
     <RegisterContainer>
     <div>
@@ -27,20 +63,22 @@ function Register() {
           <TextField
             required
             fullWidth
-            id="lastName"
+            id="username"
             label="이름"
-            name="lastName"
-            autoComplete="family-name"
+            name="username"
+            autoComplete="username"
+            onChange={(e)=> setUsernameReg(e.target.value)}
           />
         </Grid>
         <Grid item xs={12}>
           <TextField
             required
             fullWidth
-            id="email"
-            label="이메일"
-            name="email"
-            autoComplete="email"
+            id="Id"
+            label="아이디"
+            name="Id"
+            autoComplete="username"
+            onChange={(e)=> setIdReg(e.target.value)}
           />
         </Grid>
         <Grid item xs={12}>
@@ -52,19 +90,22 @@ function Register() {
             type="password"
             id="password"
             autoComplete="new-password"
+            onChange={(e)=> setPasswordReg(e.target.value)}
           />
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            name="password"
-            label="비밀번호 확인"
-            type=""
-            id="password"
-            autoComplete="new-password"
-          />
-        </Grid>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="비밀번호 확인"
+              type="password"
+              id="password-confirm"
+              autoComplete="new-password"
+              onChange={onChangePasswordConfirm}
+             
+            />
+          </Grid>
         {/* <Grid item xs={12}>
           <FormControlLabel
             control={<Checkbox value="allowExtraEmails" color="primary" />}
@@ -78,6 +119,7 @@ function Register() {
         color="secondary"
         variant="contained"
         sx={{ mt: 3, mb: 2 }}
+        onClick={userRegister}
       >
         회원가입
       </Button>
